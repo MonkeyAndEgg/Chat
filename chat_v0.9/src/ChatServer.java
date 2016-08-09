dbfd1be4683ec0a836b00934c42b26d6e24795cb
MonkeyAndEgg <nastyFrog@shuushinkais-MacBook-Air.local>
2016-08-08 23:13:10 -0700

    some bug fixed

---
+++ chat_v0.9/src/ChatServer.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.*;
+
+
+
+public class ChatServer {
+
+	public static void main(String[] args) {
+		boolean started = false; 
+		ServerSocket ss = null;
+		Socket s = null;
+		DataInputStream dis = null;
+		
+		try {
+			ss = new ServerSocket(8889);
+		}catch(BindException e){
+			System.out.println("The port is currently occupied.");
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		try{
+			started = true;
+			while(started) {
+				boolean bConnected = false;
+				s = ss.accept();
+				System.out.println("A client connected.");
+				bConnected = true;
+				dis = new DataInputStream(s.getInputStream());
+				while(bConnected){ // keep accepting msg
+					String str = dis.readUTF();   // get the message from a client
+					System.out.println(str);
+					
+				}
+				//dis.close();
+				
+			}
+		} catch (EOFException e) {	
+			System.out.println("A client closed.");		
+		} catch (IOException e) {		
+			e.printStackTrace();	
+		}finally {
+			
+			try {
+				if(dis != null) dis.close();
+				if(s != null) s.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+	}
+
+}
